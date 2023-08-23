8c12ba7d581e2c5ab26404736271070578840491
Justin Judice <jjudice51@gmail.com>
2023-08-23 15:05:56 -0500

    Initialized player set up. Added Welcome Message and basic game info.
    Added first variables, Console.Writelines and Console.Readlines to begin game.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,6 +10,25 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            // DAY ONE: Learning about variables;
+            string playerName = "Goodlsy";
+            uint playerHealth  = 100;
+            bool playerIsAlive = true;
+            string enemyName = "Baddy G. Mcevilson";
+            float WeaponDurability = 1.0f;
+            int floornumber = 0;
+
+            // DAY TWO: Welcome Messages + Player name Input after Initializing
+            Console.WriteLine("Whats good Galaxy");
+            Console.WriteLine("JuJu");
+            Console.WriteLine("Hello Whoever you are. What is your name?");
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            playerName = firstName + " " + lastName;
+            
+            //Starting to comment like Lodis said starting now. Display players name;
+            Console.WriteLine("Welcome Then "+ playerName +" ...To The Tower!");
+            Console.WriteLine("The Adventure Begins to defeat " + enemyName);
 
         }
     }
